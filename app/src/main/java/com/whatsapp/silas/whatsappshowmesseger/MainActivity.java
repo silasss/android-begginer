0699514de253a14c5aa63d9a862157a99b722a9f
silasss <silas.santos01@gmail.com>
2016-03-16 15:35:36 -0300

    Testing

--- app/src/main/java/com/whatsapp/silas/whatsappshowmesseger/MainActivity.java
+++ app/src/main/java/com/whatsapp/silas/whatsappshowmesseger/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Mensagem inicial", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
