bfc20229c5708d30aa09a9508df7f2698935bd5c
silasss <silas.santos01@gmail.com>
2016-03-21 17:57:01 -0300

    Verificando após ofuscar fonte com minify

--- app/src/main/java/com/whatsapp/silas/whatsappshowmesseger/MainActivity.java
+++ app/src/main/java/com/whatsapp/silas/whatsappshowmesseger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.whatsapp.silas.whatsappshowmesseger;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
